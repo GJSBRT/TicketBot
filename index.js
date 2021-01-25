3b77fbbd59f39894b8f5640531d5d35187768f69
GJSBRT <70862122+GJSBRT@users.noreply.github.com>
2021-01-25 18:31:34 +0100

    intial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({partials: ["MESSAGE", "USER", "REACTION"]});
+const enmap = require('enmap');
+const {token, prefix, embedcolor} = require('./config.json');
+
+const settings = new enmap({
+    name: "settings",
+    autoFetch: true,
+    cloneLevel: "deep",
+    fetchAll: true
+});
+
+client.on('ready', () => {
+    console.log('ready')
+});
+
+client.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+        if(command == "ticket-setup") {
+            let channel = message.mentions.channels.first();
+            if(!channel) return message.reply("Usage: `!ticket-setup #channel`");
+    
+            let sent = await channel.send(new Discord.MessageEmbed()
+                .setTitle("Tickets")
+                .setDescription("React to open a ticket!")
+                .setFooter("Tickets")
+                .setColor(embedcolor)
+            );
+    
+            sent.react('🎫');
+            settings.set(`${message.guild.id}-ticket`, sent.id);
+    
+            message.channel.send("Ticket Create Message Sent!")
+            console.log('Ticket Create Message Sent!');
+        }           
+    }  
+    else { 
+            message.channel.send("You\'re not an administrator.")
+        }    
+
+    if(command == "close") {
+        if(!message.channel.name.includes("ticket-")) return message.channel.send("You cannot use that here!")
+        message.channel.delete();
+    }
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    if(user.partial) await user.fetch();
+    if(reaction.partial) await reaction.fetch();
+    if(reaction.message.partial) await reaction.message.fetch();
+
+    if(user.bot) return;
+
+    let ticketid = await settings.get(`${reaction.message.guild.id}-ticket`);
+
+    if(!ticketid) return;
+
+    if(reaction.message.id == ticketid && reaction.emoji.name == '🎫') {
+        reaction.users.remove(user);
+
+        reaction.message.guild.channels.create(`ticket-${user.username}`, {
+            permissionOverwrites: [
+                {
+                    id: user.id,
+                    allow: ["SEND_MESSAGES", "VIEW_CHANNEL"]
+                },
+                {
+                    id: reaction.message.guild.roles.everyone,
+                    deny: ["VIEW_CHANNEL"]
+                }
+            ],
+            type: 'text'
+        }).then(async channel => {
+            channel.send(`<@${user.id}>`, new Discord.MessageEmbed().setTitle("Welcome to your ticket!").setDescription("We will be with you shortly. You can close the ticket with `!close`").setColor(embedcolor))
+        })
+    }
+});
+
+client.login(token);
